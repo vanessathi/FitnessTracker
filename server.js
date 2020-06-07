77084e62ef2946ac682c905dc5fe840d5b7aec44
Vanessa Pham <phamxvanessa@gmail.com>
2020-06-07 14:34:32 -0600

    merge

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3000;
 
 const app = express();
 
@@ -13,11 +13,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
-require('./routes/apiroutes.js')(app)
-require('./routes/htmlroutes.js')(app)
-
+// routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
